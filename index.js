1baef31734d5ace39301031afb973e29c395c751
Johnny Hauser <johnnyhauser@gmail.com>
2017-09-21 13:43:50 -0500

    compare with browserify, output bundle size, minified size, and gzipped size

--- index.js
+++ index.js
@@ -5,15 +5,34 @@ const getPackageDir = pify(require('get-package-dir'))
 const bundle = require('./lib/bundle')
 const minify = require('./lib/minify')
 const gzipSize = require('gzip-size')
+const browserify = require('browserify')
+const browserifyBundle = dir => pify(cb => browserify(dir).bundle(cb))()
+  .then(code => code.toString())
+const byteSize = string => Buffer.byteLength(string, 'utf8')
+
+const getSizes = async bundleCode => {
+  const minifiedCode = minify(bundleCode)
+  return {
+    code: bundleCode,
+    bytes: byteSize(bundleCode),
+    minified: byteSize(minifiedCode),
+    gzipped: await gzipSize(minifiedCode)
+  }
+}
 
 const rollupSize = async packageName => {
   const packageDir = await getPackageDir(packageName)
   const pkg = require(path.join(packageDir, 'package.json'))
   const entry = pkg.module || pkg.main || 'index.js'
   const code = await bundle(path.join(packageDir, entry))
+  const browserifyCode = await browserifyBundle(packageDir)
+
+  return {
+    rollup: await getSizes(code),
+    browserify: await getSizes(browserifyCode),
+  }
   // TODO: make gzip an option?
-    //return Buffer.byteLength(minify(code), 'utf8')
-  return await gzipSize(minify(code))
+  //return await gzipSize(minify(code))
 }
 
 module.exports = rollupSize
