e3d4cf7f1cb76e2cbfab4c5a5e835d6222f8e79f
Johnny Hauser <johnnyhauser@gmail.com>
2017-11-01 09:17:16 -0500

    bugfix for determining local entry files, refactor browserify out of main module, cli outputs rollup size even if browserify fails

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
 const path = require('path')
+const tryCatch = require('try_catch')
+const bundle = require('./lib/bundle-rollup')
+const getSizes = require('./lib/get-sizes')
+const getPackageDir = require('./lib/get-package-dir')
 
-const pify = require('pify')
-const getPackageDir = pify(require('get-package-dir'))
-const bundle = require('./lib/bundle')
-const minify = require('./lib/minify')
-const gzipSize = require('gzip-size')
-const browserify = require('browserify')
-const browserifyBundle = dir => pify(cb => browserify(dir).bundle(cb))()
-  .then(code => code.toString())
-const byteSize = string => Buffer.byteLength(string, 'utf8')
-
-const getSizes = async bundleCode => {
-  const minifiedCode = minify(bundleCode)
-  return {
-    code: bundleCode,
-    bytes: byteSize(bundleCode),
-    minified: byteSize(minifiedCode),
-    gzipped: await gzipSize(minifiedCode)
-  }
+const determineEntry = packageDir => {
+  const pkg = tryCatch(
+    () => require(path.join(packageDir, 'package.json')),
+    () => ({})
+  )
+  const pkgEntry = pkg.module || pkg.main
+  return pkgEntry
+    ? path.join(packageDir, pkgEntry)
+    : require.resolve(packageDir)
 }
 
 const rollupSize = async packageName => {
   const packageDir = await getPackageDir(packageName)
-  const pkg = require(path.join(packageDir, 'package.json'))
-  const entry = pkg.module || pkg.main || 'index.js'
-  const code = await bundle(path.join(packageDir, entry))
-  const browserifyCode = await browserifyBundle(packageDir)
-
-  return {
-    rollup: await getSizes(code),
-    browserify: await getSizes(browserifyCode),
-  }
-  // TODO: make gzip an option?
-  //return await gzipSize(minify(code))
+  const entry = determineEntry(packageDir)
+  const code = await bundle(entry)
+  return await getSizes(code)
 }
 
 module.exports = rollupSize
